f429cc4b67d72f53661ed96822e17ce1354a8600
caleb <caleb@Pelops-VI.local>
2026-07-18 12:48:58 -0400

    Update WishSim to v0.3.0

--- Formula/wishsim.rb
+++ Formula/wishsim.rb
@@ -1,16 +1,16 @@
 class Wishsim < Formula
   desc "Cinematic terminal wish simulator with animated reveals"
   homepage "https://github.com/TMAH1602/WishSim"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/TMAH1602/WishSim/releases/download/v0.2.0/wishsim-macos-arm64.tar.gz"
-      sha256 "a67dc766effeb4da58825e8737644cba83671456b1a2e333739d0709c44dac1d"
+      url "https://github.com/TMAH1602/WishSim/releases/download/v0.3.0/wishsim-macos-arm64.tar.gz"
+      sha256 "2425cbe8a4a29fbf8fe9319cae4b16e59a6a06179a856cef98e9d81c220c81bf"
     else
-      url "https://github.com/TMAH1602/WishSim/releases/download/v0.2.0/wishsim-macos-x86_64.tar.gz"
-      sha256 "11897b1cc6a6353e4c75fbf783f659d8171bd7171d1b1143fbd1de600fed7f6b"
+      url "https://github.com/TMAH1602/WishSim/releases/download/v0.3.0/wishsim-macos-x86_64.tar.gz"
+      sha256 "6befd401f7a46fa3b03999a3e5c1b2f6055c4c319c168f1d004424d5530d6a86"
     end
   end
 
